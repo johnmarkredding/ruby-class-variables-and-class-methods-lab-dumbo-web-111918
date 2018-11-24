3cbe864225541da2ab0bcd804ef2917caaeeb5cb
John Mark Redding <johnmarkredding@gmail.com>
2018-11-24 15:12:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 class Song
-  attr_accessor :name, :artist, :genre, Song::count, Song::artists, Song::genres
+  attr_accessor :name, :artist, :genre
+  Song::count, Song::artists, Song::genres
+  
   @@count = 0
   @@artists = []
   @@genres = []
